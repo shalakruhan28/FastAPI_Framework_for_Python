702f47688b5250be61b99a78e3f7942dd2414fdc
Abu Shalak Ruhan <shalakruhan1996@gmail.com>
2025-09-04 13:45:31 +0600

    Path parameters

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/blog/unpublished")
 
-@app.get("/about")
-
-def about():
-    return  {'data':{'about page'}}
+def unpublished():
+    return  {'data':'all unpublished blogs'}
 
 
-@app.get("/")
 
-def index():
-    return {'data':{'name':'hello world'}}
+@app.get("/blog/{id}")
+
+def show(id:int):
+
+    # logic to get the blog with id = id
+    return  {'data':id}
+
+
+@app.get("/blog/{id}/comments")
+    # logic to get the comments of the blog with id = id 
+def comments(id:int ):
+    return {'data':{'1','2'}}

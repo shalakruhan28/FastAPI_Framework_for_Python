69c31824a2b2283046d1c902810c0075089d2884
Abu Shalak Ruhan <shalakruhan1996@gmail.com>
2025-09-05 16:17:16 +0600

    request body,create blog

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -34,7 +36,15 @@ def Blog_page(limit=10, published:bool=True, sort: str=None):
         return  {'data':f'{limit} published blogs from the db'}
     else:
         return  {'data':f'{limit} blogs from the db'}
+
+
+class Blog (BaseModel):
+    title:str
+    body:str
+    published: Optional [bool]
     
 
-
+@app.post("/blog")
+def create_blog(blog:Blog):
+    return {'data':f'blog is created with title as {blog.title}' }
    

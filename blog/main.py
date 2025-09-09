84f1fb8d33863c6ced44feca73a47ab23d08e510
Abu Shalak Ruhan <shalakruhan1996@gmail.com>
2025-09-09 14:42:37 +0600

    Database_Connection_main.py

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI  # type: ignore
-from . import schemas # type: ignore
-    
+from fastapi import FastAPI , Depends  # type: ignore
+from . import schemas    # type: ignore
+from . import model # type: ignore 
+from .database import engine , Base , SessionLocal # type: ignore
+from sqlalchemy.orm import Session  # type: ignore
+
 app=FastAPI()
 
+model.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
+
 @app.post("/blog")
-def create_blog(request: schemas.Blog): # type: ignore
-    return request
+def create_blog(request: schemas.Blog, db:Session = Depends(get_db)): # type: ignore
+    new_blog = model.Blog(title=request.title, body=request.body, author=request.author) # type: ignore
+    db.add(new_blog)   
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+    
+

13a2d46737b325122d09752b5a915f5df91119f0
Abu Shalak Ruhan <shalakruhan1996@gmail.com>
2025-09-11 12:51:43 +0600

    Response_Model_schemas_change

--- blog/schemas.py
+++ blog/schemas.py
@@ -4,3 +4,12 @@ from pydantic import BaseModel  # type: ignore
 class Blog(BaseModel):
     title:str
     body:str
+    author:str
+    
+class ShowBlog(BaseModel):
+    title:str
+    body:str    
+    class config ():
+        orm_mode = True
+            
+            

71d7c7058a8f581a176f846535e05f5b8af48e36
Abu Shalak Ruhan <shalakruhan1996@gmail.com>
2025-09-11 13:41:07 +0600

    Creat_user_model_DB_model.py

--- blog/model.py
+++ blog/model.py
@@ -8,3 +8,11 @@ class Blog(Base):
     title = Column(String, index=True)
     body = Column(String, index=True)
     author = Column(String, index=True)
+
+class User(Base):
+    __tablename__ = "Users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String,  index=True)
+    password = Column(String)
+    
